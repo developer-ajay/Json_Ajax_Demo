4df3a30ba4ba43a44b259a721048f952c5277965
developer-ajay <developer.ajay618@gmail.com>
2025-02-25 11:08:41 +0530

    getting JSON data from URL

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click" , function () {
+
+    let ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET' , 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.onload = () => {
+     
+    let data = JSON.parse(ourRequest.responseText);
+    console.log(data);
+    
+    }
+    ourRequest.send();
+
+    });
+
+
+
