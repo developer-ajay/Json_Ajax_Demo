a4d73d37c614357b5b538f99ec8b6bca2bb05c91
developer-ajay <developer.ajay618@gmail.com>
2025-02-25 11:42:35 +0530

    hiding button on third click

--- js/index.js
+++ js/index.js
@@ -1,18 +1,35 @@
+let pageCounter = 1;
 const btn = document.getElementById("btn");
+const animalContainer = document.getElementById("animal-info");
 
 btn.addEventListener("click" , function () {
 
     let ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET' , 'https://learnwebcode.github.io/json-example/animals-1.json');
+    ourRequest.open('GET' , `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
     ourRequest.onload = () => {
      
     let data = JSON.parse(ourRequest.responseText);
-    console.log(data);
-    
+    renderHTML(data);
     }
     ourRequest.send();
+    pageCounter+= 1;
+    if (pageCounter > 3){
+        btn.classList.add("hide-me");
+    }
+});
 
-    });
+function renderHTML(data){
+      console.log(data);
+      
+      let htmlString = "";
+
+      for(i=0; i<data.length; i++ ){
+        htmlString =`<p>${data[i].name} is a ${data[i].species}</p>`
+        animalContainer.insertAdjacentHTML('beforeend' , htmlString);
+      }
+       
+      
+}
 
 
 
