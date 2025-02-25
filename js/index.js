8a237592948e3a51d2fadacc057ed0d35e2b1c7d
developer-ajay <developer.ajay618@gmail.com>
2025-02-25 12:23:57 +0530

    Final changes and touches

--- js/index.js
+++ js/index.js
@@ -7,10 +7,19 @@ btn.addEventListener("click" , function () {
     let ourRequest = new XMLHttpRequest();
     ourRequest.open('GET' , `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
     ourRequest.onload = () => {
-     
-    let data = JSON.parse(ourRequest.responseText);
-    renderHTML(data);
+    
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+        let data = JSON.parse(ourRequest.responseText);
+        renderHTML(data);
+    }else {
+        console.log("We connected to the server, but it returned an error.");
+    } 
     }
+
+    ourRequest.onerror = function() {
+        console.log("Connection error");
+    };
+
     ourRequest.send();
     pageCounter+= 1;
     if (pageCounter > 3){
@@ -23,17 +32,11 @@ function renderHTML(data){
       let htmlString = "";
 
       for (let i = 0; i < data.length; i++) {
-        let likes = data[i].foods.likes.map(food => food.toUpperCase()).join(", "); // Join likes with ", "
-        let dislikes = data[i].foods.dislikes.map(food => food.toUpperCase()).join(", "); // Join dislikes with ", "
+        let likes = data[i].foods.likes.map(food => food.toUpperCase()).join(" and "); // Join likes with ", "
+        let dislikes = data[i].foods.dislikes.map(food => food.toUpperCase()).join(" and "); // Join dislikes with ", "
         htmlString = `
             <p>${data[i].name} is a ${data[i].species} that likes to eat ${likes} and dislikes to eat ${dislikes}.</p>
-        `;
-    
+        `;    
         animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}      
 }
-       
-      
-}
-
-
-
